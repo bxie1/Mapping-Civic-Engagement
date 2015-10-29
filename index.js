d31ea7dbacb9d0c4510765c46bc39365faa6a6c1
Michael <kappeler.michael@gmail.com>
2015-10-29 13:42:36 -0400

    initial index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var express = require('express');
 var path = require('path');
-var db =  monk('localhost:27017/locations');
+var config = require('./config');
+
+//var dbUrl = 'mongodb://localhost:27017/sse';
 
 var app = new express();
 
@@ -11,7 +14,18 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res){
-  res.render('index', { title: 'Swat Social Enterprise'});
+  res.render('index', { title: 'Returns all'});
+});
+
+app.get('/api/v1/', function(req,res){
+  MongoClient.connect(config.dbUrl, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db.collection('locations').find({}).toArray(function(err, docs) {
+        res.json(docs);
+        db.close();
+    });
+  });
 });
 
 app.listen(3000)
